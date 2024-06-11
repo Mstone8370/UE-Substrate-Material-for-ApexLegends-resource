907ad49e66b22ccf5e628a4abae35145fc8123b7
mstone8370 <mstone8370@gmail.com>
2024-06-11 16:15:48 +0900

    Update Plugin and PostProcessVolume

--- Source/ApexLegendsProjectEditor.Target.cs
+++ Source/ApexLegendsProjectEditor.Target.cs
@@ -9,7 +9,8 @@ public class ApexLegendsProjectEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
-		ExtraModuleNames.AddRange( new string[] { "ApexLegendsProject" } );
+        ExtraModuleNames.AddRange( new string[] { "ApexLegendsProject" } );
 	}
 }
