66944203a044d18ac0d36d9a620c341fdb8d27f1
mstone8370 <mstone8370@gmail.com>
2025-11-14 13:14:17 +0900

    Update to 5.7

--- Source/ApexLegendsProject.Target.cs
+++ Source/ApexLegendsProject.Target.cs
@@ -8,7 +8,7 @@ public class ApexLegendsProjectTarget : TargetRules
 	public ApexLegendsProjectTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "ApexLegendsProject" } );
 	}
--- Source/ApexLegendsProjectEditor.Target.cs
+++ Source/ApexLegendsProjectEditor.Target.cs
@@ -8,7 +8,7 @@ public class ApexLegendsProjectEditorTarget : TargetRules
 	public ApexLegendsProjectEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
         ExtraModuleNames.AddRange( new string[] { "ApexLegendsProject" } );
